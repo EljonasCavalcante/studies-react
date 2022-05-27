0f33d1a55d824224d7f4f9b4e12f9dfca415c4b9
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-05-26 23:24:10 -0300

    feat:transforming dynamic code

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 
 function List() {
+    const Tarefa = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    },
+    {
+        tarefa: 'Javascript',
+        tempo: '01:00:00'
+
+    },
+    {
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+    }]
     return(
+        
         <aside>
             <h2>Estudo do dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Javascript
-                    </h3>
-                    <span>
-                        03:00:00
-                    </span>
-                </li>
+                {Tarefa.map((item, index) => (
+                    <li key={index}>
+                        <h3>{item.tarefa}</h3>
+                        <span>{item.tempo}</span>
+                    </li>
+                ))}       
             </ul>
         </aside>
 
